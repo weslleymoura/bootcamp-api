9896465e36eba0f9365c79d1c21054d7d7150ed0
Weslley Moura <weslleymoura@gmail.com>
2024-08-15 15:40:51 +0100

    new deploymet

--- bkp/main.py
+++ bkp/main.py
@@ -1,28 +1,19 @@
-import os
-
-# Variáveis de ambiente
-os.environ['AWS_ACCESS_KEY_ID'] = 'user'
-os.environ['AWS_SECRET_ACCESS_KEY'] = 'password'
-os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://localhost:9000'
-os.environ['DATABASE_URL'] = 'postgresql://bootcamp_user:admin@localhost:5434/bootcamp_db'
-
 from fastapi import FastAPI, HTTPException
-from src.delivery_region_mlflow import DeliveryRegion
+from src.delivery_region import DeliveryRegion
 import brazilcep
 from geopy.geocoders import Nominatim
 import geopy.distance
 from joblib import load
 from pathlib import Path
 import sys 
-import mlflow
 path = Path().joinpath().joinpath('..')
 sys.path.append(str(path))
+
 from src.database import SessionLocal, engine
 from src import models, schemas
 from sqlalchemy.orm import Session
 from fastapi import Query, Depends
 
-# Prepara o banco de dados
 models.Base.metadata.create_all(bind=engine)
 
 def get_db():
@@ -32,19 +23,11 @@ def get_db():
     finally:
         db.close() 
 
-# MLFlow server
-mlflow.set_tracking_uri("http://localhost:5000")
-
-# Instancia um objeto da classe DeliveryRegion, que contém algumas lógicas da API
-delivery_region = DeliveryRegion()
-
-# Carrega a versão de produção do modelo
-model_name = 'prod.bootcamp.kmeans-clustering'
-alias = 'champion'
-model_uri, run_id = delivery_region.get_model_uri_by_name_and_alias(model_name, alias)
-loaded_model = mlflow.pyfunc.load_model(model_uri)
-
 app = FastAPI()
+
+
+
+delivery_region = DeliveryRegion()
 
 @app.get("/", summary="Bootcamp root parh")
 async def bootcamp():
@@ -53,14 +36,31 @@ async def bootcamp():
 @app.get("/get-delivery-region/{lat}/{lng}", summary="Valida região de entrega")
 def get_delivery_region(lat, lng, db: Session = Depends(get_db)):
 
-    model_uri, run_id = delivery_region.get_model_uri_by_name_and_alias(model_name, alias)
-    loaded_model = mlflow.pyfunc.load_model(model_uri)
+    # Prepara as coordenadas do CEP
+    coords_cep = (lat, lng)
 
-    data = {
-        'method': 'predict',
-        'data': [lat, lng]
-    } 
-    result = loaded_model.predict(data)
+    # Lista centróides dos clusters
+    centers = delivery_region.get_cluster_centroids(delivery_region.model)
+
+    # Verifica a distância (em km) entre o CEP a cada cluster
+    res = dict()
+    for c in centers:
+        coords_cluster = (c['lat'], c['lng'])
+        res[c['cluster']] = geopy.distance.geodesic(coords_cep, coords_cluster).km
+
+    # Cria uma lista com as chaves (keys) ordenadas pelos seus valores (values)
+    res_sorted_keys = sorted(res, key=res.get, reverse=False)
+
+    # Prepara o resultado do endpoint
+    result = {
+        'is_region_covered': round(res[res_sorted_keys[0]], 2) <= delivery_region.covered_region_in_km,
+        'closest_center': {
+            'id': res_sorted_keys[0],
+            'distance_in_km': round(res[res_sorted_keys[0]], 2),
+            'lat': centers[res_sorted_keys[0]]['lat'],
+            'lng': centers[res_sorted_keys[0]]['lng']
+        }
+    }
 
     # Registra a chamada da API no banco de dados
     new_api_call = models.ApiCall(
@@ -86,23 +86,18 @@ def get_delivery_region(lat, lng, db: Session = Depends(get_db)):
 @app.get("/get-model-version", summary="Retorna a versão da API")
 def get_model_version():
 
-    model_uri, run_id = delivery_region.get_model_uri_by_name_and_alias(model_name, alias)
-    loaded_model = mlflow.pyfunc.load_model(model_uri)
+    result = {
+        'version': delivery_region.version
+    }
 
-    data = {
-        'method': 'get_model_version',
-        'data': None
-    } 
-    return loaded_model.predict(data)
+    return result
 
 @app.get("/get-clusters", summary="Retorna os centróides de cada cluster")
 def get_cluster():
 
-    model_uri, run_id = delivery_region.get_model_uri_by_name_and_alias(model_name, alias)
-    loaded_model = mlflow.pyfunc.load_model(model_uri)
+    return delivery_region.get_cluster_centroids(delivery_region.model)
 
-    data = {
-        'method': 'get_cluster_centroids',
-        'data': None
-    } 
-    return loaded_model.predict(data)
+@app.get("/get-sample-points", summary="Retorna alguns pontos (lat, lng) de exemplo para teste da aplicação")
+def get_sample_points():
+
+    return load(delivery_region.sample_points_location)
